b86ee5c809875b91c85800fd014ea3590f690e56
Karin Ochayon <karinoch070@gmail.com>
2023-06-15 12:50:29 +0300

    Changing syntax

--- src/models/usersmodel.js
+++ src/models/usersmodel.js
@@ -20,7 +20,7 @@ const usersSchema = new mongoose.Schema(
   { versionKey: false }
 );
 
-// Creating a Mongoose model named "usersModel" for the "users" collection in the MongoDB database using the defined "usersSchema".
+// Creating a Mongoose model named "usersModel" for the "users" collection in the MongoDB database using the defined "usersSchema"
 const usersModel = mongoose.model("users", usersSchema);
 
 // Exporting the usersModel as a module
